016c383a2d28418003c031cb8754f69208f8ed3e
Ann-Wi <vasileva.ana@outlook.com>
2020-04-26 12:37:01 +0300

    broken out homepage into smaller components, finished styling out menu-item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -18,7 +18,7 @@ class Directory extends React.Component {
                 title: 'jackets',
                 imageUrl: 'http://i.ibb.co/px2tCc3/jackets.png',
                 id: 2
-            }
+            },
             {
                 title: 'sneakers',
                 imageUrl: 'http://i.ibb.co/0jqHpnp/sneakers.png',
@@ -27,10 +27,12 @@ class Directory extends React.Component {
             {
                 title: 'womens',
                 imageUrl: 'http://i.ibb.co/GCCdy8t/womens.png',
+                size: 'large',
                 id: 4
             },
             {title: 'mens',
                 imageUrl: 'http://i.ibb.co/R70vBrQ/men.png',
+                size: 'large',
                 id: 5
             }]
         }
@@ -40,8 +42,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id}) => (
-                        <MenuItem key={id} title={title} />
+                    this.state.sections.map(({ title, imageUrl, id, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                     ))}
             </div>
         );
