01476ca89e4c0e375a1d797d9c7386a100376c71
Ann-Wi <vasileva.ana@outlook.com>
2020-07-15 13:50:28 +0300

    added redux, and implemented userReducer and userActions to header and app components

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import customButton from '../custom-button/custom-button.component';
 
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
